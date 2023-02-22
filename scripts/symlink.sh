83226a2827f715682f7d212b19aa934caa39b497
Admilson Marques <admilson.marques@nubank.com.br>
2023-02-21 22:42:43 -0300

    symlink

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -3,6 +3,6 @@
 git_folder="../klipper/config/"
 dest_folder="$HOME/printer_data/"
 echo "creating symlink ${git_folder} -> ${dest_folder}"
-ln -sfn "${git_folder}" "${dest_folder}"
+ln -sfn  "${dest_folder}" "${git_folder}"
 
 
