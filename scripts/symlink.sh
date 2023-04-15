e7425f91c23dc6cc054fc72498ac2c1d42a86545
Admilson Marques <admilsonmarques@gmail.com>
2023-04-15 00:33:01 -0300

    update deploy script

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -2,7 +2,10 @@
 
 git_folder="$HOME/dev/ade/klipper-cfg/klipper/config/"
 dest_folder="$HOME/printer_data/"
+secrets_origin="$HOME/.secrets/moonraker.secrets"
+secrets_dest="$dest_folder/moonraker.secrets"
 echo "creating symlink ${git_folder} -> ${dest_folder}"
 ln -sfn "${git_folder}" "${dest_folder}"
-
+echo "creating symlink fro secrets"
+ln -sfn "${secrets_origin}" "${secrets_dest}"
 
